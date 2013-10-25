c8175ecaeaf99de2f9adb50bf1686aa3248af0f2
cis <cis@CISM262.(none)>
2013-10-25 17:45:39 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NivoDemo::Application.config.secret_token = 'b5f2e16f7744d5c46845779438058a97e2e03d2f38abfc4c8ecf0535ef9ce4b46e9388ebce94655829a8fa6c33de980d41e9a4308fc0535c60d8f5955bab8a4c'
